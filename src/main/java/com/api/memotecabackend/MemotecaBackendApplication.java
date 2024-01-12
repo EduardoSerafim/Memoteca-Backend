c136eac3554b8ac14acdab515ea108e038082239
Eduardo Serafim <eduserafim7@gmail.com>
2024-01-11 22:43:04 -0300

    Initial commit

---
+++ src/main/java/com/api/memotecabackend/MemotecaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.memotecabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemotecaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemotecaBackendApplication.class, args);
+	}
+
+}
